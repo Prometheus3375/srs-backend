ff84031cf3849a21ad8cc5f6bb9654b2a22d3347
Dragos Strugar <itsdragos@gmail.com>
2020-04-24 17:36:15 +0300

    csv functionality export

--- backend/csvMaker.py
+++ backend/csvMaker.py
@@ -1,7 +1,17 @@
 from typing import List
+import csv
+import io
 
-Filename = 'export.csv'  # filename to suggest
+Filename = 'export.csv'
 
 
 def make(data: List[dict]) -> str:
-    return ''
+    with io.StringIO() as csvfile:
+        fieldnames = data.keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+        return csvfile.getvalue()
+
