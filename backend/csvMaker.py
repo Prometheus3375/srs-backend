e255af103336be3e27c95286744a36301a7106d5
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-05 19:30:40 +0300

    Updated csvMaker.py

--- backend/csvMaker.py
+++ backend/csvMaker.py
@@ -1,18 +1,40 @@
 from typing import List
 import csv
 import io
+import re
 
 Filename = 'export.csv'
+_spaces_pattern = re.compile(r'\s+')
 
 
-def make(data: List[dict]) -> str:
+def replace(text: str, pattern: re, repl: str) -> str:
+    it = re.finditer(pattern, text)
+    out = ''
+    i = 0
+    for m in it:
+        start = m.start()
+        out += text[i:start] + repl
+        i = m.end()
+    if i < len(text):
+        out += text[i:]
+    return out
+
+
+def make(itemlist: List[dict]) -> str:
     # set newline to None to make newlines are written as \n on all platforms
     with io.StringIO() as csvfile:
-        fieldnames = set().union(*(d.keys() for d in data))
+        fieldnames = set().union(*(obj.keys() for obj in itemlist))
         fieldnames = sorted(fieldnames)
         writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
 
         writer.writeheader()
-        for row in data:
-            writer.writerow(row)
+        for obj in itemlist:
+            for key, value in obj.items():
+                if value is None:
+                    value = ''
+                elif isinstance(value, list):
+                    value = ' '.join([str(v) for v in value])
+                value = replace(str(value), _spaces_pattern, ' ')
+                obj[key] = value
+            writer.writerow(obj)
         return csvfile.getvalue()
