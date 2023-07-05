c7b09a99be4018f348798b39f330f7f7635a7cbd
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-05 19:28:02 +0300

    Tested with front, there is communication Removed unnecessary apps, may include later. Added sample json response.

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect, HttpResponseBadRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.decorators import api_view
@@ -8,63 +8,50 @@ from django.contrib.sessions.backends.base import SessionBase
 import json
 
 
-@api_view(['GET', 'POST'])
-def index(request: Request):
+def index(request: HttpRequest):
     session: SessionBase = request.session
+    print(f"Remote IP: {request.META['REMOTE_ADDR']}")
     if request.method == 'GET':
         session['data'] = []
-        return Response()
-    elif request.method == 'POST':
-        data = [
-            {
-                'name': 'Super notebook',
-                'price': '1000$',
-                'sites': ['amazon', 'e-bay'],
-            },
-            {
-                'name': 'Not a super notebook',
-                'price': '10$',
-                'sites': ['amazon', 'e-bay'],
-            },
-        ]
+        return HttpResponse('This is main page')
+    if request.method == 'POST':
+        print(request.POST)
+        # session.cycle_key()
+        with open('backend/sample.json', 'r', encoding = 'utf-8') as f:
+            data = json.load(f)
         session['data'] = data
-        return Response(data)
+        return HttpResponse('This is main page')
+    else:
+        print(f'{session} sent {request.method} request')
+        return HttpResponseBadRequest()
 
 
-@api_view(['GET'])
-def results(request: Request):
+def results(request: HttpRequest):
     session: SessionBase = request.session
-    data = session['data']
-    return Response(data)
-
-# def index(request: HttpRequest):
-#     session: SessionBase = request.session
-#     if request.method == 'POST':
-#         # session.cycle_key()
-#         data = [
-#             {
-#                 'name': 'Super notebook',
-#                 'price': '1000$',
-#                 'sites': ['amazon', 'e-bay'],
-#             },
-#             {
-#                 'name': 'Not a super notebook',
-#                 'price': '10$',
-#                 'sites': ['amazon', 'e-bay'],
-#             },
-#         ]
-#         session['data'] = data
-#         return HttpResponse('This is main page')
-#     else:
-#         print(f'{session} sent {request.method} request')
-#         return HttpResponseBadRequest()
+    if request.method == 'GET':
+        data = session['data']
+        return HttpResponse(json.dumps(data), content_type = 'application/json')
+    else:
+        print(f'{session} sent {request.method} request')
+        return HttpResponseBadRequest()
 
 
-# def results(request: HttpRequest):
+# @api_view(['GET', 'POST'])
+# def index(request: Request):
 #     session: SessionBase = request.session
 #     if request.method == 'GET':
-#         data = session['data']
-#         return HttpResponse(json.dumps(data))
-#     else:
-#         print(f'{session} sent {request.method} request')
-#         return HttpResponseBadRequest()
+#         session['data'] = []
+#         return Response()
+#     elif request.method == 'POST':
+#         print(request.POST)
+#         with open('backend/sample.json', 'r', encoding = 'utf-8') as f:
+#             data = json.load(f)
+#         session['data'] = data
+#         return Response(data)
+#
+#
+# @api_view(['GET'])
+# def results(request: Request):
+#     session: SessionBase = request.session
+#     data = session['data']
+#     return Response(data)
